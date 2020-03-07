e245db1106d35d0a3b24eee4e175b1c7d07f82ef
时空路由器 <chinesejar@gmail.com>
2020-03-07 18:36:10 +0800

    Remove useless code in getCommunityDetail.js

--- getCommunityDetail.js
+++ getCommunityDetail.js
@@ -8,52 +8,6 @@ dotenv.config({
 
 const { Community } = require('./entities');
 
-getDistrict = async (city) => {
-  console.log(`开始获取 ${city.city} 的区数据...`);
-  const districts = [];
-  let res = await axios.get(`${city.href}/xiaoqu`);
-  const $ = cheerio.load(res.data);
-  $('div[data-role=ershoufang]').find('div').find('a').each(function () {
-    districts.push({ href: $(this).attr('href'), name: $(this).text(), city: city.id });
-  });
-  console.log(`获取到 ${city.city} 的 ${districts.length} 个区数据。`);
-  return districts;
-}
-
-getBizCircle = async (city, district) => {
-  console.log(`开始获取 ${city.city} 的 ${district.name}区 的商圈数据...`);
-  const bizCircles = [];
-  let res = await axios.get(`${city.href}${district.href}`);
-  const $ = cheerio.load(res.data);
-  $('div[data-role=ershoufang]').find('div').next('div').find('a').each(function () {
-    bizCircles.push({ href: $(this).attr('href'), name: $(this).text(), district: district.id });
-  });
-  console.log(`获取到 ${city.city} 的 ${district.name}区 的 ${bizCircles.length} 个商圈数据。`);
-  return bizCircles;
-}
-
-getCommunity = async (city, bizCircle, page = 1) => {
-  const communities = [];
-  let res = await axios.get(`${city.href}${bizCircle.href}pg${page}`);
-  const $ = cheerio.load(res.data);
-  $('ul.listContent').find('li').each(function () {
-    const a = $(this).find('a');
-    communities.push({
-      href: a.attr('href'), img: a.find('img').attr('src'), name: a.attr('title'),
-      code: $(this).attr('data-id'), biz_circle: bizCircle.id
-    });
-  });
-  let pageInfo = $('div.page-box.house-lst-page-box').attr('page-data');
-  // 如果该商圈无小区，返回空数组
-  if (pageInfo) pageInfo = JSON.parse(pageInfo);
-  else return [];
-  if (pageInfo.totalPage === pageInfo.curPage) {
-    return communities;
-  } else {
-    return communities.concat(await getCommunity(city, bizCircle, page + 1));
-  }
-}
-
 (async () => {
   const queries = await Community.findAll();
   for (const query of queries) {
